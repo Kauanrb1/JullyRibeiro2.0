e2f0345183e5231a226ccdd7492b591b0fe418fe
Kauanrb1 <kauanribeirofsa@gmail.com>
2025-05-22 00:46:04 -0300

    Comecei a mexer em algumas coisas do carrinho

--- js/cart.js
+++ js/cart.js
@@ -1,30 +1,28 @@
 function addToCart(event) {
-    const product = event.target.parentNode;
-    const productName = product.querySelector('h2').innerText;
-    const productImage = product.querySelector('img').src;
-    const productPrice = product.querySelector('p:nth-child(3)').innerText;
-  
-    // Cria um objeto para representar o produto
-    const productData = {
-      name: productName,
-      image: productImage,
-      price: productPrice
-    };
-  
-    // Obtém o carrinho atual do armazenamento local ou cria um novo array vazio
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-  
-    // Adiciona o produto ao carrinho
-    cart.push(productData);
-  
+  const product = event.target.parentNode;
+  const productName = product.querySelector('.produtoNome').innerText;
+  const productImage = product.querySelector('.card-img-top').src; 
+  const productPrice = product.querySelector('.p:nth-of-type(2)').innerText;
 
-  
-    // Salva o carrinho atualizado de volta no armazenamento local
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
-  
-  const buyButtons = document.querySelectorAll('.buy-btn');
-  
-  buyButtons.forEach(button => {
+  // Cria um objeto para refencar o produto
+  const productData ={
+    name: productName,
+    image: productImage,
+    price: productPrice
+  };
+
+  //Obtém o carrinho atual do localStorage ou cria um novo carrinho vazio
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+  // Adiciona o produto ao carrinho
+  cart.push(productData);
+
+  // Salva o carrinho atualizado no localStorage
+  localStorage.setItem('cart', JSON.stringify(cart));
+  };
+
+  const buyButton = document.querySelectorAll('.btnComprar');
+
+  buyButton.forEach(button => {
     button.addEventListener('click', addToCart);
   });
